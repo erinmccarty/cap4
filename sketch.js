74a87252792e1767666f5a4cd761c1b9334576e1
Erin McCarty <58147518+erinmccarty@users.noreply.github.com>
2022-03-03 18:27:54 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -77,10 +77,10 @@ function draw(){
   
   
   blendMode(BLEND);
-  image(cam, -500, -400, 800, 600);
+  image(cam, -400, -300, 800, 600);
   
   blendMode(MULTIPLY);
-  image(vid, -500, -400, 850, 600);
+  image(vid, -400, -300, 850, 600);
   
   
 }
